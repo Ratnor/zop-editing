8957fc777acd836530b54cb055b9a513c43b772e
Akshay Mantha <Akshay Mantha>
2015-11-27 19:04:11 -0500

    tile class for testing

---
+++ source/testing/Tile.py
@@ -0,0 +1,10 @@
+import random
+class Tile:
+
+    def __init__(self):
+        color = ["R","B","G","P","Y"]
+        self.color = random.choice(color)
+
+    def getColor(self):
+        return self.color
+
